dc50bfcad57f11c16c8a441da8130edc92758eda
José Moreno <jomoespe@gmail.com>
2019-12-13 09:31:22 +0100

    Execute init in a code block

--- web/js/index.js
+++ web/js/index.js
@@ -2,14 +2,16 @@
 // Use of this source code is governed by a The Unlicense license that can be
 // found in the LICENSE file.
 
-// Instantiate Go runtime and register the WASM module
-const go = new Go();
-console.log("Loading main.wasm");
-WebAssembly.instantiateStreaming(fetch("js/main.wasm"), go.importObject)
-    .then((result) => {
-        console.log("main.wasm loaded");
-        go.run(result.instance);
-    });
+(() => {
+    // Instantiate Go runtime and register the WASM module
+    const go = new Go();
+    console.log("Loading main.wasm");
+    WebAssembly.instantiateStreaming(fetch("js/main.wasm"), go.importObject)
+        .then((result) => {
+            console.log("main.wasm loaded");
+            go.run(result.instance);
+        });
 
-// event handler that will call a function in WASM
-document.querySelector("#button-1").onclick = event => goFunction("from JS -> WASM")
+    // event handler that will call a function in WASM
+    document.querySelector("#button-1").onclick = event => goFunction("from JS -> WASM")
+})();
